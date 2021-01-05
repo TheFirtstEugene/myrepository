a81a79cf6fd8a5b90007cb36ed6910f01ef95e10
Евгений <loolka98@gmail.com>
2021-01-06 00:02:33 +0200

    Homework_2

--- src/lesson2/Task3_1.java
+++ src/lesson2/Task3_1.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Task3_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int x1;
+        int x2;
         System.out.println("Введите поочерёдно переменные а, b и c");
         int a = sc.nextInt();
         System.out.println("a = " + a);
@@ -11,13 +13,20 @@ public class Task3_1 {
         int c = sc.nextInt();
         System.out.println("b = " + c);
         int d = (b*b)-(4*a*c);
-        int x1 = (int)(-b+Math.sqrt(d))/2*a;
-        int x2 = (int)(-b-Math.sqrt(d))/2*a;
-        System.out.println("Корни равны :");
-        if(x1>x2)
-            System.out.println(x2+" "+x1);
-        else System.out.println(x1 + " "+ x2);
-
+        if(d>0){
+            x1 = (int) ((-b+Math.sqrt(d))/(2*a));
+            x2 = (int) ((-b-Math.sqrt(d))/(2*a));
+            System.out.println("Корни равны :");
+            if(x1>x2)
+                System.out.println(x2+" "+ x1);
+            else System.out.println(x1 + " "+ x2);
+        }
+        else if(d==0){
+            x1 = -b/(2*a);
+            System.out.println("У данного примера всего один корень: " + x1 + ", так как дискриминант равен 0");
+        } else {
+            System.out.println("Дискриминант меньше нуля. У данного уравнения нет корней");
+        }
 
 
     }
